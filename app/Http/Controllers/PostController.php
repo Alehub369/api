05039991a7fc600c2936f8333ebc3def8cbbda1e
Alehub369 <lievanoalejandro@gmail.com>
2023-11-22 00:46:32 -0500

    Sitio web: completando la estructura

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index() 
     {
         return view('index', [
             'posts' => Post::latest()->paginate()
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,23 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index']);
 
 /* Route::get('/', function () {
     return view('welcome');
 }); */
+
+
+/*
+Con esta Ruta no hay necesita de controlador
+
+Route::get('/', function () {
+    return view('index', [
+        'posts' => Post::get()
+    ]);
+});
+ */
+
+ Route::get('/', [PostController::class, 'index']);
+
+
+ 
