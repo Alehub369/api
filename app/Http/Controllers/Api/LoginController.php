f7e797ce5e4872b00f1936f6e5a3ae70ca76db18
Alehub369 <lievanoalejandro@gmail.com>
2023-11-23 14:15:24 -0500

    Acceso con autenticacion

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->getValidationFactory($request);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Version 1
+
+/* Route::post('v1/posts', function (Request $request) {
+   $token = $request->user()->createToken($request->token_name);
+
+   return ['token' => $token->plainTextToken];
+}); */
+
+
 
  Route::apiResource('v1/posts', PostV1::class)
     ->only(['index', 'show', 'destroy'])
@@ -16,3 +26,8 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('v2/posts', PostV2::class)
    ->only(['index', 'show'])
    ->middleware('auth:sanctum');
+
+Route::post('/login', [
+   LoginController::class,
+   'login'
+]);
